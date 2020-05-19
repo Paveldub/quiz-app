18bb206b227a98153f07c93becf550c09cb902af
Pavel dub <dubstr1@gmail.com>
2020-05-19 18:16:34 +0300

    input Component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,41 @@
 import React, {Component} from 'react';
+import classes from './auth.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/input'
 
 export default class Auth extends Component {
+
+    loginHandler = () => {
+        console.log('Login')
+    }
+    
+    registerHandler = () => {
+        console.log('register')
+    }
+
+    submitHandler = (e) => {
+       e.preventDefault()
+    }
+
     render() {
         return (
-            <div>
-                <h1>Auth page</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <Input label='Email' type='text' errorMessage='No Email'/>
+                        <Input label='Password' type='text' />
+
+                        <Button type='success' onClick={this.loginHandler}>
+                            Войти
+                        </Button>
+
+                        <Button type='primary' onClick={this.registerHandler}>
+                            Зарегистрироваться
+                        </Button>
+                    </form>
+                </div>
             </div>
         )
     }
